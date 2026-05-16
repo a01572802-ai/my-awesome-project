98e781406f1d92b7d2373e3cbccf6541ea749a86
Axel Corral <a01572802@tec.mx>
2026-05-16 15:26:06 -0600

    Primera funcion con modulos

---
+++ def_weather.py
@@ -0,0 +1,40 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv(".env")
+
+
+def get_weather_tempt_city(city_name):
+    city = city_name
+    api_key = os.getenv("API_KEY")
+    url_city = (
+        f"http://api.openweathermap.org/geo/1.0/direct?q={city}&limit=1&appid={api_key}"
+    )
+
+    try:
+        city_data = requests.get(url_city)
+        city_data.raise_for_status
+        city_data_json = city_data.json()
+        if not city_data_json:
+            print(f"No se encontró la ciudad: {city_name}")
+            return
+        lat = city_data_json[0]["lat"]
+        lon = city_data_json[0]["lon"]
+    except requests.exceptions.RequestException as e:
+        print(f"Error al buscar la ciudad: {e}")
+        return
+
+    url_data = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=metric"
+
+    try:
+        response = requests.get(url_data)
+        response.raise_for_status()
+        data = response.json()
+        print(f"Ciudad: {city.capitalize()}, {data['sys']['country']}")
+        print(f"Temperatura: {data['main']['temp']:.1f}")
+        print(f"Clima: {data['weather'][0]['main']}")
+    except requests.exceptions.HTTPError as e:
+        print(f"Error de API: {e}")
+    except requests.exceptions.ConnectionError:
+        print("Sin conexión a internet")
--- weather.py
+++ weather.py
@@ -1,23 +1,3 @@
-import requests
-from dotenv import load_dotenv
-import os
+from def_weather import get_weather_tempt_city
 
-
-load_dotenv(".env")
-api_key = os.getenv("API_KEY")
-lat = 25.755746
-lon = -100.411714
-
-url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=metric"
-
-try:
-    response = requests.get(url)
-    response.raise_for_status()
-    data = response.json()
-    print(f"Ciudad: Monterrey, {data['sys']['country']}")
-    print(f"Temperatura: {data['main']['temp']:.1f}")
-    print(f"Clima: {data['weather'][0]['main']}")
-except requests.exceptions.HTTPError as e:
-    print(f"Error de API: {e}")
-except requests.exceptions.ConnectionError:
-    print("Sin conexión a internet")
+city = get_weather_tempt_city(input("City name: "))
