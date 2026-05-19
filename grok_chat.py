2278bc96fccd151c4cba1fcf80d26d8cea81b972
Axel Corral <a01572802@tec.mx>
2026-05-19 17:55:31 -0600

    Preferencias de respuesta groq

--- grok_chat.py
+++ grok_chat.py
@@ -1,4 +1,18 @@
 from def_grok import preguntar
 
-respuesta = preguntar(input("Que le quieres preguntar a Groq?"))
-print(respuesta)
+with open("system_prompt.txt", "r") as file:
+    system_prompt = file.read()
+
+historial = [{"role": "system", "content": system_prompt}]
+
+while True:
+    mensaje = input("Tú: ")
+
+    if mensaje == "salir":
+        break
+
+    historial.append({"role": "user", "content": mensaje})
+    respuesta = preguntar(historial)
+    historial.append({"role": "assistant", "content": respuesta})
+
+    print(f"IA: {respuesta}\n")
