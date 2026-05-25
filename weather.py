7afaeeba146bfb37a41ba13de6514f352d0398ef
Axel Corral <a01572802@tec.mx>
2026-05-25 03:39:42 -0600

    liberando-espacio

--- weather.py
+++
@@ -1,7 +0,0 @@
-from def_weather import get_weather_tempt_city
-
-resultado = get_weather_tempt_city(input("City name: "))
-if resultado:
-    print(f"Ciudad: {resultado['ciudad']}, {resultado['pais']}")
-    print(f"Temperatura: {resultado['temp']:.1f}°C")
-    print(f"Clima: {resultado['descripcion']}")
