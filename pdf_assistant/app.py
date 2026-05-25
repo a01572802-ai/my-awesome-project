ac7a0e9fa0076fc61c0702221110efe90e85a0a2
Axel Corral <a01572802@tec.mx>
2026-05-25 17:38:48 -0600

    add historial and prepare for deploy

--- pdf_assistant/app.py
+++ pdf_assistant/app.py
@@ -1,9 +1,17 @@
 import streamlit as st
+import sys
+import os
+sys.path.append(os.path.dirname(__file__))
+
 from pdf_processor import leer_pdf, dividir_chunks
 from vector_store import guardar_chunks, buscar_chunks, get_collection
 from llm import responder
+
 st.title("📄 Asistente de PDFs")
 st.write("Sube un PDF y hazle preguntas")
+
+if "historial" not in st.session_state:
+    st.session_state.historial = []
 
 pdf = st.file_uploader("Sube tu PDF", type="pdf")
 
@@ -21,9 +29,15 @@ if pdf:
             with st.spinner("Buscando respuesta..."):
                 contexto = buscar_chunks(pregunta, collection)
                 respuesta = responder(pregunta, contexto)
-                st.write(respuesta)
+                
+                st.session_state.historial.append({
+                    "pregunta": pregunta,
+                    "respuesta": respuesta
+                })
         else:
             st.warning("Escribe una pregunta primero")
 
-
-
+    for item in st.session_state.historial:
+        st.write(f"**Tú:** {item['pregunta']}")
+        st.write(f"**IA:** {item['respuesta']}")
+        st.divider()
--- pdf_assistant/pdf_processor.py
+++ pdf_assistant/pdf_processor.py
@@ -8,10 +8,11 @@ def leer_pdf(archivo):
         texto_completo += pagina.extract_text()
     return texto_completo
 
+
 def dividir_chunks(texto, chunk_size=500):
     chunks = []
     for i in range(0, len(texto), chunk_size):
-        chunk = texto[i:i + chunk_size]
+        chunk = texto[i : i + chunk_size]
         if chunk.strip():
             chunks.append(chunk)
     return chunks
